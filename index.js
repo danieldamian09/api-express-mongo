3fee1df03ca623c81605bf678bed97c15e0284a4
danieldamian09 <humbertorivero9@gmail.com>
2022-04-19 22:26:53 -0300

    route /test

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-// console.log(express)
+//API Router
+import prodcutRouter from "./routes/api.js";
 
 const app = express();
+
+app.use("/api/products", prodcutRouter);
 
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
 
